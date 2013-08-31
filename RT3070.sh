b1e4d577dd9da348d49afb59d2036f7836536f21
Owner <owner@localhost.localdomain>
2013-08-31 13:17:24 -0500

    fix?

--- RT3070.sh
+++ RT3070.sh
@@ -4,7 +4,7 @@ owd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" #Path to THIS script.
 #lsusb = Bus 001 Device 004: ID 148f:3070 Ralink Technology, Corp. RT2870/RT3070 Wireless Adapter
 #http://www.ebay.com/itm/320986973768		~$4
 mkdir "$owd/pkgs"
-sudo apt-get --no-install-recommends  -do dir::cache::archives="$owd/pkgs" install linux-headers* build-essential checkinstall wpasupplicant
+sudo apt-get --no-install-recommends  -do dir::cache::archives="$owd/pkgs" install linux-headers* build-essential checkinstall wpasupplicant linux-headers-3.6-trunk-rpi linux-image-3.6-trunk-rpi
 sudo dpkg -i $owd/pkgs/*.deb
 cd $owd
 tar -xjf 2011_0719_RT3070_RT3370_RT5370_RT5372_Linux_STA_V2.5.0.3_DPO.bz2
@@ -14,7 +14,7 @@ patch -s -p0 < 2011_0719_RT3070_RT3370_RT5370_RT5372_Linux_STA_V2.5.0.3_DPOfix.p
 #To fix "/lib/modules/*/build: No such file or directory. Stop." errors modify
 #the driver Makefile to point to '/usr/src/linux-headers-3.6-trunk-rpi' rather
 #than '/lib/modules/$(shell uname -r)'.
-sed -i 's/\/lib\/modules\/\$(shell uname -r)/\/usr\/src\/linux-headers-3.6-trunk-rpi/' Makefile
+sed -i 's/\/lib\/modules\/\$(shell uname -r)/\/lib\/modules\/3.6-trunk-rpi/' Makefile
 sudo make && sudo checkinstall -D make install
 make clean
 cd $owd
