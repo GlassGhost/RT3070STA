9d3d36e8bfde8df827862256c81b83dcc13ba4c6
Owner <owner@localhost.localdomain>
2013-09-03 03:07:11 -0500

    Signed-off-by: Owner <owner@localhost.localdomain>

---
+++ 3.6.11+build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+owd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" #Path to THIS script.
+#this script is to provide the "/lib/modules/3.6.11+/build" directory and fix "/lib/modules/3.6.11+/build: No such file or directory. Stop." errors
+
+cd /usr/src
+sudo wget https://github.com/raspberrypi/linux/archive/rpi-3.6.y.tar.gz
+sudo tar xvfz rpi-3.6.y.tar.gz
+sudo ln -s /usr/src/linux-rpi-3.6.y/ /lib/modules/3.6.11+/build
+cd /lib/modules/3.6.11+/build
+sudo make mrproper
+sudo gzip -dc /proc/config.gz > .config
+sudo su
+gzip -dc /proc/config.gz > .config
+make modules_prepare
+wget https://github.com/raspberrypi/firmware/raw/master/extra/Module.symvers
+exit
--- RT3070.sh
+++ RT3070.sh
@@ -4,21 +4,19 @@ owd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" #Path to THIS script.
 #lsusb = Bus 001 Device 004: ID 148f:3070 Ralink Technology, Corp. RT2870/RT3070 Wireless Adapter
 #http://www.ebay.com/itm/320986973768		~$4
 mkdir "$owd/pkgs"
-sudo apt-get --no-install-recommends  -do dir::cache::archives="$owd/pkgs" install linux-headers* build-essential checkinstall wpasupplicant linux-headers-3.6-trunk-rpi linux-image-3.6-trunk-rpi
+sudo apt-get --no-install-recommends  -do dir::cache::archives="$owd/pkgs" install linux-headers* build-essential checkinstall wpasupplicant
 sudo dpkg -i $owd/pkgs/*.deb
+#To fix "/lib/modules/*/build: No such file or directory. Stop." errors
+sudo bash $owd/3.6.11+build.sh
+
 cd $owd
 tar -xjf 2011_0719_RT3070_RT3370_RT5370_RT5372_Linux_STA_V2.5.0.3_DPO.bz2
 cd $owd/2011_0719_RT3070_RT3370_RT5370_RT5372_Linux_STA_V2.5.0.3_DPO
 #From http://forum.stmlabs.com/showthread.php?tid=8333
 patch -s -p0 < 2011_0719_RT3070_RT3370_RT5370_RT5372_Linux_STA_V2.5.0.3_DPOfix.patch
-#To fix "/lib/modules/*/build: No such file or directory. Stop." errors modify
-#the driver Makefile to point to '/usr/src/linux-headers-3.6-trunk-rpi' rather
-#than '/lib/modules/$(shell uname -r)'.
-#sed -i 's/\/lib\/modules\/\$(shell uname -r)/\/lib\/modules\/3.6-trunk-rpi/' Makefile
 make clean && make && sudo checkinstall -D make install
-#
-cd $owd
-#rm -rf 2011_0719_RT3070_RT3370_RT5370_RT5372_Linux_STA_V2.5.0.3_DPO
+
+#rm -rf $owd/2011_0719_RT3070_RT3370_RT5370_RT5372_Linux_STA_V2.5.0.3_DPO
 
 sudo echo "blacklist rt2x00usb
 blacklist rt2x00lib
