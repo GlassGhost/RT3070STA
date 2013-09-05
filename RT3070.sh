2bfe9255e16fcf70508528b1ee66631125f38039
Owner <owner@localhost.localdomain>
2013-09-05 03:21:22 -0500

    Signed-off-by: Owner <owner@localhost.localdomain>

--- RT3070.sh
+++ RT3070.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 owd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" #Path to THIS script.
-
+#sudo chmod -R 777 ./* && git checkout -- . && git clean -f -x -d
 #lsusb = Bus 001 Device 004: ID 148f:3070 Ralink Technology, Corp. RT2870/RT3070 Wireless Adapter
 #http://www.ebay.com/itm/320986973768		~$4
 mkdir "$owd/pkgs"
 sudo apt-get -y --no-install-recommends  -do dir::cache::archives="$owd/pkgs" install linux-headers-3.6* checkinstall wpasupplicant
 sudo dpkg -i $owd/pkgs/*.deb
 #To fix "/lib/modules/*/build: No such file or directory. Stop." errors
-sudo ln -s /lib/modules/3.6-trunk-rpi/ /lib/modules/3.6.11+/build
+sudo rm /lib/modules/3.6.11+/build
+sudo ln -s /usr/src/linux-headers-3.6-trunk-rpi /lib/modules/3.6.11+/build
 cd /lib/modules/3.6.11+/build
 sudo wget https://github.com/raspberrypi/firmware/raw/master/extra/Module.symvers
-sudo gzip -dc /proc/config.gz | sudo tee .config > /dev/null
+sudo gzip -dc /proc/config.gz | sudo tee /lib/modules/3.6.11+/build/.config > /dev/null
 
 cd $owd
+sudo rm -rf $owd/2011_0719_RT3070_RT3370_RT5370_RT5372_Linux_STA_V2.5.0.3_DPO
 tar -xjf 2011_0719_RT3070_RT3370_RT5370_RT5372_Linux_STA_V2.5.0.3_DPO.bz2
-cd $owd/2011_0719_RT3070_RT3370_RT5370_RT5372_Linux_STA_V2.5.0.3_DPO
 #From http://forum.stmlabs.com/showthread.php?tid=8333
 patch -s -p0 < 2011_0719_RT3070_RT3370_RT5370_RT5372_Linux_STA_V2.5.0.3_DPOfix.patch
-make clean && make
-sudo checkinstall --pkgname='rt5370sta' --pkgversion='2.5.0.3' -y -D make install
+cd $owd/2011_0719_RT3070_RT3370_RT5370_RT5372_Linux_STA_V2.5.0.3_DPO
+make clean && make && sudo checkinstall --pkgname='rt5370sta' --pkgversion='2.5.0.3' -y -D make install
 #--exclude='/lib/modules/3.6.11+/modules.devname'
 #rm -rf $owd/2011_0719_RT3070_RT3370_RT5370_RT5372_Linux_STA_V2.5.0.3_DPO
 
