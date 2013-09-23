d9a7797cbd70f2737238dad10f9ee64dd7c79431
Owner <owner@localhost.localdomain>
2013-09-23 03:32:31 -0500

    Signed-off-by: Owner <owner@localhost.localdomain>

--- 3.6.11+build.sh
+++ 3.6.11+build.sh
@@ -1,15 +1,44 @@
 #!/bin/bash
+sst=`date -u "+%FT%TZ"` #Script Start Time
 owd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" #Path to THIS script.
-#this script is to provide the "/lib/modules/3.6.11+/build" directory and fix "/lib/modules/3.6.11+/build: No such file or directory. Stop." errors
+#   Copyright 2013 Roy Pfund
+#
+#   Licensed under the Apache License, Version 2.0 (the  "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable  law  or  agreed  to  in  writing,
+#   software distributed under the License is distributed on an  "AS
+#   IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,  either
+#   express or implied. See the License for  the  specific  language
+#   governing permissions and limitations under the License.
+#_______________________________________________________________________________
+# This script is to provide the "/lib/modules/3.6.11+/build" directory and fix
+# "/lib/modules/3.6.11+/build: No such file or directory. Stop." errors
 
 cd /usr/src
-sudo wget https://github.com/raspberrypi/linux/archive/rpi-3.6.y.tar.gz
+
+if [-s /usr/src/xvfz rpi-3.6.y.tar.gz]; then #/usr/src/xvfz rpi-3.6.y.tar.gz file exists
+	echo "/usr/src/xvfz rpi-3.6.y.tar.gz exists"
+else #/usr/src/xvfz rpi-3.6.y.tar.gz file  doesn't exist
+	sudo wget https://github.com/raspberrypi/linux/archive/rpi-3.6.y.tar.gz
+fi
+
+if [-d /usr/src/linux-rpi-3.6.y]; then #/usr/src/linux-rpi-3.6.y Directory exists
+	echo "deleting /usr/src/linux-rpi-3.6.y"
+	sudo rm -rf /usr/src/linux-rpi-3.6.y
+else #/usr/src/linux-rpi-3.6.y Directory doesn't exist
+	
+fi
 sudo tar xvfz rpi-3.6.y.tar.gz
 sudo ln -s /usr/src/linux-rpi-3.6.y/ /lib/modules/3.6.11+/build
+
 cd /lib/modules/3.6.11+/build
+sudo make mrproper
 sudo wget https://github.com/raspberrypi/firmware/raw/master/extra/Module.symvers
 sudo gzip -dc /proc/config.gz | sudo tee .config > /dev/null
 sudo make oldconfig
-sudo make mrproper
 sudo make modules_prepare
 exit
--- RT3070.sh
+++ RT3070.sh
@@ -1,18 +1,44 @@
 #!/bin/bash
+sst=`date -u "+%FT%TZ"` #Script Start Time
 owd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" #Path to THIS script.
+#   Copyright 2013 Roy Pfund
+#
+#   Licensed under the Apache License, Version 2.0 (the  "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable  law  or  agreed  to  in  writing,
+#   software distributed under the License is distributed on an  "AS
+#   IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,  either
+#   express or implied. See the License for  the  specific  language
+#   governing permissions and limitations under the License.
+#_______________________________________________________________________________
 
 #lsusb = Bus 001 Device 004: ID 148f:3070 Ralink Technology, Corp. RT2870/RT3070 Wireless Adapter
 #http://www.ebay.com/itm/320986973768		~$4
-mkdir "$owd/pkgs"
-sudo apt-get -y --no-install-recommends  -do dir::cache::archives="$owd/pkgs" install build-essential checkinstall wpasupplicant
-sudo dpkg -i $owd/pkgs/*.deb
+
+if [-d $owd/pkgs]; then #$owd/pkgs Directory exists
+	echo "$owd/pkgs exists & prolly installed"
+else #$owd/pkgs Directory doesn't exist
+	mkdir "$owd/pkgs"
+	sudo apt-get -y --no-install-recommends  -do dir::cache::archives="$owd/pkgs" install build-essential checkinstall wpasupplicant
+	sudo dpkg -i $owd/pkgs/*.deb
+fi
+
 #To fix "/lib/modules/*/build: No such file or directory. Stop." errors
 sudo bash $owd/3.6.11+build.sh
 
 cd $owd
-tar -xjf 2011_0719_RT3070_RT3370_RT5370_RT5372_Linux_STA_V2.5.0.3_DPO.bz2
+if [-d $owd/2011_0719_RT3070_RT3370_RT5370_RT5372_Linux_STA_V2.5.0.3_DPO]; then #$owd/2011_0719_RT3070_RT3370_RT5370_RT5372_Linux_STA_V2.5.0.3_DPO Directory exists
+	echo "$owd/2011_0719_RT3070_RT3370_RT5370_RT5372_Linux_STA_V2.5.0.3_DPO exists"
+	sudo rm -rf $owd/2011_0719_RT3070_RT3370_RT5370_RT5372_Linux_STA_V2.5.0.3_DPO
+else #$owd/2011_0719_RT3070_RT3370_RT5370_RT5372_Linux_STA_V2.5.0.3_DPO Directory doesn't exist
+	tar -xjf 2011_0719_RT3070_RT3370_RT5370_RT5372_Linux_STA_V2.5.0.3_DPO.bz2
+fi
 cd $owd/2011_0719_RT3070_RT3370_RT5370_RT5372_Linux_STA_V2.5.0.3_DPO
-#From http://forum.stmlabs.com/showthread.php?tid=8333
+#Patch From http://forum.stmlabs.com/showthread.php?tid=8333
 patch -s -p0 < 2011_0719_RT3070_RT3370_RT5370_RT5372_Linux_STA_V2.5.0.3_DPOfix.patch
 make clean && make
 sudo checkinstall --pkgname='rt5370sta' --pkgversion='2.5.0.3' -y -D make install
